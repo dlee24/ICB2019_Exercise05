3bafd0945c59a39790cb255a7c6e16d3b1282718
Diane Lee <dlee24@nd.edu>
2019-10-10 18:51:29 -0400

    Parts1&2

--- wages.sh
+++ wages.sh
@@ -2,17 +2,12 @@
 #usage: bash wages.sh
 #Logan Thayer and Diane Lee's script
 
+cat wages.csv | grep -v 'gender' | grep -E 'female' | sort -t , -nk2 -u | cut -d , -f1,2 | sed -E 's/,/ /g' > uniqcombos.txt
+cat wages.csv | grep -v 'gender' | grep -E '^male' | sort -t , -nk2 -u | cut -d , -f1,2 | sed -E 's/,/ /g' >> uniqcombos.txt
 
-
-
-
-
-
-
-echo "gender,yearsExperience,yearsSchool,wage"
-echo "Highest Earner"
+echo "Highest and Lowest Earners, Respectively"
+echo "Gender,YearsExp,Wage"
 cat wages.csv | grep -v "gender" | sort -t , -nrk4 | cut -d , -f 1,2,4 | head -n 1
-echo "Lowest Earner"
 cat wages.csv | grep -v "gender" | sort -t , -nrk4 | cut -d , -f 1,2,4 | tail -n 1
 echo "Number of Women in Top 10 Earners:"
 cat wages.csv | grep -v "gender" | sort -t , -nrk4 | head -n 10 | grep -E "female" | wc -l
