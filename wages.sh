413fa5f7ff034627bd36cb28f77352c14eb37df0
loganthayer1 <logan.thayer@gmail.com>
2019-10-10 18:16:59 -0400

    Logan's portion number 2

--- wages.sh
+++ wages.sh
@@ -1,3 +1,19 @@
 #!bin/sh
-#usage
+#usage:
+#Logan Thayer and Diane Lee's script
 
+
+
+
+
+
+
+
+
+echo "gender,yearsExperience,yearsSchool,wage"
+echo "Highest Earner"
+cat wages.csv | grep -v "gender" | sort -t , -nrk4 | cut -d , -f 1,2,4 | head -n 1
+echo "Lowest Earner"
+cat wages.csv | grep -v "gender" | sort -t , -nrk4 | cut -d , -f 1,2,4 | tail -n 1
+echo "Number of Women in Top 10 Earners:"
+cat wages.csv | grep -v "gender" | sort -t , -nrk4 | head -n 10 | grep -E "female" | wc -l
